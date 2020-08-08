e54833abfde93f9b3ae2decf521382fa48f8dd0d
gemmaseeley <61994787+gemmaseeley@users.noreply.github.com>
2020-08-08 14:06:53 +1000

    front end skeleton added

---
+++ eaglesolutions/src/pages/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Landing() {
+    
+    return (
+        <div className="landing-container">
+            <h1>
+                Eagle Solutions
+            </h1>
+        </div>
+    );
+}
