48f62ebfa43a0e6db100f5036f6033f7e8daa3c0
glennp1 <61995051+glennp1@users.noreply.github.com>
2020-09-22 12:06:35 +1000

    added backend skeleton for portfolio

--- backend/controllers/portfolioControllers.js
+++ backend/controllers/portfolioControllers.js
@@ -1,11 +1,37 @@
 // import libraries
 const mongoose = require('mongoose');
+const Portfolio = mongoose.model('portfolios');
 const Account = mongoose.model('accounts');
 
 const contactInfo = function(req, res, next) {
     console.log(req.body)
   };
 
+// todo get portfolios with specific account id
+
+const readByAccountId = function(req, res, next) {
+
+};
+
+const readOne = function(req, res, next) {
+
+}
+
+const updateByAccountId = function(req, res, next) {
+
+};
+
+const deleteByAccountId = function(req, res, next) {
+
+};
+
+// useful link
+// https://stackoverflow.com/questions/8737082/mongoose-schema-within-schema
+
 module.exports = {
-    contactInfo
+    contactInfo,
+    readByAccountId,
+    readOne,
+    updateByAccountId,
+    deleteByAccountId
 }
--- backend/routes/portfolioRoutes.js
+++ backend/routes/portfolioRoutes.js
@@ -4,7 +4,26 @@ const portfolioController = require('../controllers/portfolioControllers');
 
 // CREATE
 
+// todo should be called when an account is created
 portfolioRoutes.post('/contactInfo', portfolioController.contactInfo);
 
-//
+// READ
+
+// todo readByAccountId
+portfolioRoutes.get('/readByAccountId', portfolioController.readByAccountId)
+
+// todo readOne
+portfolioRoutes.get('/readOne', portfolioController.readOne)
+
+// UPDATE
+
+// todo updateByAccountId
+portfolioRoutes.put('/updateByAccountId', portfolioController.updateByAccountId)
+
+// DELETE
+
+// todo should be called when an account is deleted
+// todo deleteByAccountId
+portfolioRoutes.delete('/deleteByAccountId', portfolioController.deleteByAccountId);
+
 module.exports = portfolioRoutes;
