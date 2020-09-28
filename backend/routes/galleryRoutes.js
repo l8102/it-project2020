d309aff2257ef60914bad8d0f8b543defeb2abd2
l8102 <ml.l8102@gmail.com>
2020-09-28 19:50:33 +1000

    Update galleryRoutes.js

--- backend/routes/galleryRoutes.js
+++ backend/routes/galleryRoutes.js
@@ -3,11 +3,15 @@ const galleryRoutes = express.Router();
 const galleryControllers = require('../controllers/galleryControllers');
 
 // CREATE
+galleryRoutes.post('/addGallery', galleryControllers.createGallery);
 
 // READ
+galleryRoutes.get('/readGallery', galleryControllers.readGallery);
 
 // UPDATE
+galleryRoutes.put('/updateGallery', galleryControllers.updateGallery);
 
 // DELETE
+galleryRoutes.delete('/deleteGallery', galleryControllers.deleteGallery);
 
 module.exports = galleryRoutes;
