8fc4b348842a6e235e5cb801aed38ec27a84ef3e
l8102 <ml.l8102@gmail.com>
2020-09-28 19:44:05 +1000

    Update aboutRoutes.js

--- backend/routes/aboutRoutes.js
+++ backend/routes/aboutRoutes.js
@@ -3,11 +3,15 @@ const aboutRoutes = express.Router();
 const aboutControllers = require('../controllers/aboutControllers');
 
 // CREATE
+aboutRoutes.post('/addAbout', aboutControllers.createAbout);
 
 // READ
+aboutRoutes.get('/readAbout', aboutControllers.readAbout);
 
 // UPDATE
+aboutRoutes.put('/updateAbout', aboutControllers.updateAbout);
 
 // DELETE
+aboutRoutes.delete('/deleteAbout', aboutRoutes.deleteAbout);
 
 module.exports = aboutRoutes;
