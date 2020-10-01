56954f287836d3f8dfe80b4baeac2c83797c532e
l8102 <62783953+l8102@users.noreply.github.com>
2020-10-01 14:43:05 +1000

    added update aboutMe controller & route

--- backend/routes/aboutRoutes.js
+++ backend/routes/aboutRoutes.js
@@ -2,16 +2,17 @@ const express = require('express');
 const aboutRoutes = express.Router();
 const aboutControllers = require('../controllers/aboutControllers');
 
-// // CREATE
-// // handled by create account
-//
-// // READ
-// aboutRoutes.get('/readAbout', aboutControllers.readAbout);
-//
-// // UPDATE
-// aboutRoutes.put('/updateAbout', aboutControllers.updateAbout);
-//
-// // DELETE
-// // handled by delete account
+// CREATE
+// handled by create account
+
+// READ
+//aboutRoutes.get('/readAbout', aboutControllers.readAbout);
+
+// UPDATE
+aboutRoutes.put('/updateAboutMe', aboutControllers.updateByAccountId);
+
+
+// DELETE
+// handled by delete account
 
 module.exports = aboutRoutes;
