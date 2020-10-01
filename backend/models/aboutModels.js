da4248eaab9153de17d05ca3cdd97f66b84b5ecb
l8102 <62783953+l8102@users.noreply.github.com>
2020-10-01 14:43:11 +1000

    Update aboutModels.js

--- backend/models/aboutModels.js
+++ backend/models/aboutModels.js
@@ -3,10 +3,38 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const aboutSchema = new Schema({
-  accountId: {
-    type: String,
-    required: true
-  },
+    accountId: {
+        type: String,
+        required: true
+    },
+
+
+    institution: {
+        type: String
+    },
+    degree: {
+        type: String
+    },
+    major: {
+        type: String
+    },
+
+
+    description: {
+        type: String
+    },
+
+    interests: {
+        type: [String],
+        value: [String],
+        default: undefined
+    },
+
+    workExperience: {
+        type: { experience: String, dateFrom: Date, dateTo: Date },
+        value: [{ experience: String, dateFrom: Date, dateTo: Date }],
+    },
+    
 }, {
   collection: 'About',
   timestamps: true,
