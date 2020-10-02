0f9027131fe434591112a388c4fbb941dc28793e
gemmaseeley <gseeley@student.unimelb.edu.au>
2020-10-02 16:08:09 +1000

    formatting

--- frontend/src/pages/pcomponents/EditGallery.js
+++ frontend/src/pages/pcomponents/EditGallery.js
@@ -6,8 +6,6 @@ export default function Upload() {
     const [fileState, setFileState] = useState('');
     const [preview, setPreview] = useState('');
     const [selectedFile, setSelectedFile] = useState();
-    const [successMsg, setSuccessMsg] = useState('');
-    const [errMsg, setErrMsg] = useState('');
 
     //when the user changes the file change state 
     const fileChange = (e) => {
@@ -29,15 +27,17 @@ export default function Upload() {
     //
     const handleSubmitFile = (event) => {
         event.preventDefault();
-        if (!selectedFile) return;
+        
+        if (!selectedFile) 
+            return;
+        
         const reader = new FileReader();
         reader.readAsDataURL(selectedFile);
         reader.onloadend = () => {
             uploadImage(reader.result);
         };
         reader.onerror = () => {
-            console.error('AHHHHHHHH!!');
-            setErrMsg('something went wrong!');
+            console.error('error on submit');
         };
     };
 
@@ -47,17 +47,13 @@ export default function Upload() {
             const upload = await uploadAPI(base64EncodedImage);
             setFileState('');
             setPreview('');
-            setSuccessMsg('Image uploaded successfully');
         } catch (err) {
             console.error(err);
-            setErrMsg('Something went wrong!');
         }
     };
     return (
         <div className="pcontainer">
             <h1 className="title">Upload Gallery Images</h1>
-            <Alert msg={errMsg} type="danger" />
-            <Alert msg={successMsg} type="success" />
             <form onSubmit={handleSubmitFile} className="account-form gallery-form">
                 <input
                     id="fileInput"
@@ -80,17 +76,4 @@ export default function Upload() {
             )}
         </div>
     );
-}
-
-function Alert({ msg, type }) {
-    const [show, setShow] = useState(false);
-    useEffect(() => {
-        if (msg) {
-            setShow(true);
-            setInterval(() => {
-                setShow(false);
-            }, 2000);
-        }
-    }, [msg]);
-    return <>{show && <div className={`alert alert-${type}`}>{msg}</div>}</>;
 }
