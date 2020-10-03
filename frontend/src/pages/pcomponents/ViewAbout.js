310537a281748597107c7ee32a580ee96c5e8649
glennp1 <61995051+glennp1@users.noreply.github.com>
2020-10-03 12:11:31 +1000

    viewabout bugfix

--- frontend/src/pages/pcomponents/ViewAbout.js
+++ frontend/src/pages/pcomponents/ViewAbout.js
@@ -64,111 +64,112 @@ export default class ViewAbout extends Component {
     render() {
         if (!this.state.isLoaded) {
             return (
-                <div>
-                    Loading...
-                </div>
+              <div>
+                  Loading...
+              </div>
             )
         } else {
             return (
-                <div className="about-me-page">
-                    <h1>
-                        View About
-                </h1>
-                    <section>
-                        <h2>
-                            Educational Background
-                    </h2>
-                        <label>
-                            Institution:
-                    </label>
-                        <p>
-                            {this.state.institution}
-                        </p>
-                        <label>
-                            Degree:
-                    </label>
-                        <p>
-                            {this.state.major}
-                        </p>
-                        <label>
-                            Major:
-                    </label>
-                        <p>
-                            {this.state.major}
-                        </p>
-                    </section>
-                    <section>
-                        <h2>
-                            Work Experience/Internships
-                    </h2>
+              <div className="about-me-page">
+                  <h1>
+                      View About
+                  </h1>
+                  <section>
+                      <h2>
+                          Educational Background
+                      </h2>
+                      <label>
+                          Institution:
+                      </label>
+                      <p>
+                          {this.state.institution}
+                      </p>
+                      <label>
+                          Degree:
+                      </label>
+                      <p>
+                          {this.state.major}
+                      </p>
+                      <label>
+                          Major:
+                      </label>
+                      <p>
+                          {this.state.major}
+                      </p>
+                  </section>
+                  <section>
+                      <h2>
+                          Work Experience/Internships
+                      </h2>
 
-                    {
-                        <div className="list-table">
-                            <table>
-                                <tbody>
-                                    <tr>
-                                        <th>
-                                            Experience:
-                                        </th>
-                                        <th>
-                                            Date Started:
-                                        </th>
-                                        <th>
-                                            Date Finished:
-                                        </th>
-                                    </tr>
-                                    {this.state.experienceList.map((x, i) => {
-                                        return (
-                                            <tr key={i}>
-                                                <td className="experience-col">
-                                                    {x.experience}
-                                                </td>
-                                                <td>
-                                                    {x.dateFrom}
-                                                </td>
-                                                <td>
-                                                    {x.dateTo}
-                                                </td>
-                                            </tr>
-                                        )
-                                    })
-                                    }
-                                </tbody>
-                            </table>
-                        </div>
-                    }
-                </section>
-                <section>
-                    <h2>
-                        Interests
-                    </h2>
-                    {
-                        <div className="list-table">
-                            <table>
-                                <tbody>
-                                    {this.state.interestList.map((x, i) => {
-                                        return (
-                                            <tr key={i}>
-                                                <td>
-                                                    {x}
-                                                </td>
-                                            </tr>
-                                        )
-                                    })}
-                                </tbody>
-                            </table>
-                        </div>
-                    }
-                </section>
-                <section>
-                    <h2>
-                        About 'Name'
-                    </h2>
-                    <p className="description">
-                        {this.state.description}
-                    </p>
-                </section>
-            </div>
-        )
+                      {
+                          <div className="list-table">
+                              <table>
+                                  <tbody>
+                                  <tr>
+                                      <th>
+                                          Experience:
+                                      </th>
+                                      <th>
+                                          Date Started:
+                                      </th>
+                                      <th>
+                                          Date Finished:
+                                      </th>
+                                  </tr>
+                                  {this.state.experienceList.map((x, i) => {
+                                      return (
+                                        <tr key={i}>
+                                            <td className="experience-col">
+                                                {x.experience}
+                                            </td>
+                                            <td>
+                                                {x.dateFrom}
+                                            </td>
+                                            <td>
+                                                {x.dateTo}
+                                            </td>
+                                        </tr>
+                                      )
+                                  })
+                                  }
+                                  </tbody>
+                              </table>
+                          </div>
+                      }
+                  </section>
+                  <section>
+                      <h2>
+                          Interests
+                      </h2>
+                      {
+                          <div className="list-table">
+                              <table>
+                                  <tbody>
+                                  {this.state.interestList.map((x, i) => {
+                                      return (
+                                        <tr key={i}>
+                                            <td>
+                                                {x}
+                                            </td>
+                                        </tr>
+                                      )
+                                  })}
+                                  </tbody>
+                              </table>
+                          </div>
+                      }
+                  </section>
+                  <section>
+                      <h2>
+                          About 'Name'
+                      </h2>
+                      <p className="description">
+                          {this.state.description}
+                      </p>
+                  </section>
+              </div>
+            )
+        }
     }
 }
