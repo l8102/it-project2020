0483a8a64e39277d3cd49110acadbdd6d442fad6
glennp1 <61995051+glennp1@users.noreply.github.com>
2020-10-23 23:52:53 +1100

    Hard coded access code implemented

--- frontend/src/pages/EnterAccessCode.js
+++ frontend/src/pages/EnterAccessCode.js
@@ -1,20 +1,69 @@
 import React, {Component} from 'react';
 // import "../css/EnterAccessCode.css";
+import { withRouter } from 'react-router-dom';
 // import SearchResult from "../components/SearchResult";
 // import {getAllAccountsByFullName, getAllAccounts} from "../Api";
 
 class EnterAccessCode extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      accessCode: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  render() {
+  // Method to handle change in input
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  // Method to handle form submission
+  async handleSubmit(e) {
+    e.preventDefault();
+
+    let res;
+
+    // todo get access code from database
+    //   will be generated at portfolio creation
+
+    const accessCode = "123"
+
+    const accountIdTemp = sessionStorage.getItem("accountIdTemp");
+
+    if (this.state.accessCode === accessCode) {
+      // Store the account id that the user can view
+      sessionStorage.setItem("accountIdForView", accountIdTemp);
+      // Navigate to the view portfolio page
+      this.props.history.push("/viewPortfolio");
+    } else {
+      alert("Incorrect access code");
+    }
+  }
+
+    render() {
     return (
       <div>
-        Enter Access Code
+        <h1 className="private-access-title" >
+          This portfolio is private. Please enter the access code below.
+        </h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="password"
+            name="accessCode"
+            value={this.state.accessCode}
+            onChange={this.handleChange}
+          />
+          <button type="submit" className="access-code-btn">
+            Enter
+          </button>
+        </form>
       </div>
     );
   }
 }
 
-export default EnterAccessCode;
+export default withRouter(EnterAccessCode);
