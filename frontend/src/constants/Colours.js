7445e538297b8e47414534fa7cfcd708e116efb0
glennp1 <61995051+glennp1@users.noreply.github.com>
2020-10-25 16:54:44 +1100

    Added colour to portfolio model, fixed default colours

--- frontend/src/constants/Colours.js
+++ frontend/src/constants/Colours.js
@@ -1,32 +1,37 @@
+import "../css/DefaultStyles.css"
+
+// import the styles from the css
+let style = getComputedStyle(document.getElementById('root'));
+
 export const colours = {
   "blue": {
-    "light": "#c0d0d7",
-    "mid": "#347b98",
-    "dark": "#10465b"
+    "light": style.getPropertyValue('--light-blue'),
+    "mid": style.getPropertyValue('--mid-blue'),
+    "dark": style.getPropertyValue('--dark-blue')
   },
   "green": {
-    "light": "#c9dbd5",
-    "mid": "#239d99",
-    "dark": "#176966"
+    "light": style.getPropertyValue('--light-green'),
+    "mid": style.getPropertyValue('--mid-green'),
+    "dark": style.getPropertyValue('--dark-green')
   },
   "yellow": {
-    "light": "#d0caa8",
-    "mid": "#cdb920",
-    "dark": "#796918"
+    "light": style.getPropertyValue('--light-yellow'),
+    "mid": style.getPropertyValue('--mid-yellow'),
+    "dark": style.getPropertyValue('--dark-yellow')
   },
   "orange": {
-    "light": "#d7b89a",
-    "mid": "#dc711b",
-    "dark": "#90480e"
+    "light": style.getPropertyValue('--light-orange'),
+    "mid": style.getPropertyValue('--mid-orange'),
+    "dark": style.getPropertyValue('--dark-orange')
   },
   "red": {
-    "light": "#c99c9c",
-    "mid": "#c91919",
-    "dark": "#6c1212"
+    "light": style.getPropertyValue('--light-red'),
+    "mid": style.getPropertyValue('--mid-red'),
+    "dark": style.getPropertyValue('--dark-red')
   },
   "purple": {
-    "light": "#be9fc4",
-    "mid": "#ab17c7",
-    "dark": "#530f61"
+    "light": style.getPropertyValue('--light-purple'),
+    "mid": style.getPropertyValue('--mid-purple'),
+    "dark": style.getPropertyValue('--dark-purple')
   }
 }
