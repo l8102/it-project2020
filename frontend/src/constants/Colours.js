97f5a6818debaf814ad2f90354073d130d38c257
glennp1 <61995051+glennp1@users.noreply.github.com>
2020-10-25 13:55:19 +1100

    Creation of colours constants

---
+++ frontend/src/constants/Colours.js
@@ -0,0 +1,32 @@
+export const colours = {
+  "blue": {
+    "light": "#c0d0d7",
+    "mid": "#347b98",
+    "dark": "#10465b"
+  },
+  "green": {
+    "light": "#c9dbd5",
+    "mid": "#239d99",
+    "dark": "#176966"
+  },
+  "yellow": {
+    "light": "#d0caa8",
+    "mid": "#cdb920",
+    "dark": "#796918"
+  },
+  "orange": {
+    "light": "#d7b89a",
+    "mid": "#dc711b",
+    "dark": "#90480e"
+  },
+  "red": {
+    "light": "#c99c9c",
+    "mid": "#c91919",
+    "dark": "#6c1212"
+  },
+  "purple": {
+    "light": "#be9fc4",
+    "mid": "#ab17c7",
+    "dark": "#530f61"
+  }
+}
