cd5f0d439de5d4ade3015f26a870504c3eb56562
glennp1 <61995051+glennp1@users.noreply.github.com>
2020-10-27 01:13:46 +1100

    Reformated a gallery and links

--- frontend/src/pages/pcomponents/EditGallery.js
+++ frontend/src/pages/pcomponents/EditGallery.js
@@ -1,108 +1,102 @@
-import React, { Component } from 'react';
-import { uploadAPI } from "../../Api.js";
+import React, {Component} from 'react';
+import {uploadAPI} from "../../Api.js";
 import "../../css/Gallery.css";
-import { getImages } from '../../Api.js'
+import {getImages} from '../../Api.js'
 import ViewGallery from './ViewGallery.js';
 import ViewEditButton from "../../components/ViewEditButton"
 
 
 export default class EditGallery extends Component {
 
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          selectedFile: "",
-          images: [""]
-        }
-    
-        this.fileChange = this.fileChange.bind(this);
-        this.renderUpdate = this.renderUpdate.bind(this);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedFile: "",
+      images: [""]
     }
 
-    async componentDidMount() {
+    this.fileChange = this.fileChange.bind(this);
+    this.renderUpdate = this.renderUpdate.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.renderUpdate();
+  }
+
+  async fileChange(e) {
+    let file = e.target.files[0];
+    console.log(file);
+    await this.setState({selectedFile: file});
+    console.log(this.state.selectedFile);
+  }
+
+  async handleSubmitFile(event) {
+    event.preventDefault();
+
+    if (!this.state.selectedFile)
+      return;
+
+    const reader = new FileReader();
+    reader.readAsDataURL(this.state.selectedFile);
+    reader.onloadend = () => {
+      this.uploadImage(reader.result);
+      //sessionStorage.setItem("activeTab", this.props.name);
+    };
+    reader.onerror = () => {
+      console.error('error on submit');
+    };
+  };
+
+  //stores the image in the database
+  async uploadImage(base64EncodedImage) {
+    try {
+      const upload = await uploadAPI(base64EncodedImage);
+
+      // after it is done uploading, render the update
       await this.renderUpdate();
-    }
-    
-    async fileChange(e) {
-        let file = e.target.files[0];
-        console.log(file);
-        await this.setState({selectedFile: file});
-        console.log(this.state.selectedFile);
-    }
-
-    async handleSubmitFile(event) {
-        event.preventDefault();
-        
-        if (!this.state.selectedFile) 
-            return;
-        
-        const reader = new FileReader();
-        reader.readAsDataURL(this.state.selectedFile);
-        reader.onloadend = () => {
-            this.uploadImage(reader.result);
-            //sessionStorage.setItem("activeTab", this.props.name);
-        };
-        reader.onerror = () => {
-            console.error('error on submit');
-        };
-    };
-
-    //stores the image in the database 
-    async uploadImage(base64EncodedImage) {
-        try {
-            const upload = await uploadAPI(base64EncodedImage);
-
-            // after it is done uploading, render the update
-            await this.renderUpdate();
-            this.setState({
-                selectedFile : ""
-            });
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    handleButtonChange() {
-        if (this.isToggleOn) {
-
-        }
-    }
-
-    async renderUpdate() {
-      const res = await getImages();
-      const imageUrls = [];
-
-      res.forEach((image, index) => {
-        if (image.imageUrl) {
-          imageUrls[index] = image.imageUrl;
-        }
-      });
-
       this.setState({
-        images : imageUrls
+        selectedFile: ""
       });
-      console.log(this.state.images);
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    render() {
-        return (
-            <div className="pcontainer">
-                <h1 className="title">Upload Gallery Images</h1>
-                <form onSubmit={e => this.handleSubmitFile(e)} className="gallery-form">
-                    <input
-                        type="file"
-                        name="image"
-                        onChange={e => this.fileChange(e)}
-                    />
-                    <button className="save-btn" type="submit">
-                        Upload
-                    </button>
-                </form>
-                <RenderImages images={this.state.images}/>
-            </div>
-        );
-    }
+  async renderUpdate() {
+    const res = await getImages();
+    const imageUrls = [];
+
+    res.forEach((image, index) => {
+      if (image.imageUrl) {
+        imageUrls[index] = image.imageUrl;
+      }
+    });
+
+    this.setState({
+      images: imageUrls
+    });
+    console.log(this.state.images);
+  }
+
+  render() {
+    return (
+      <div className="pcontainer">
+        <h1 className="title">Upload Gallery Images</h1>
+        <form onSubmit={e => this.handleSubmitFile(e)} className="gallery-form">
+          <input
+            type="file"
+            name="image"
+            onChange={e => this.fileChange(e)}
+          />
+          <button className="save-btn" type="submit">
+            Upload
+          </button>
+        </form>
+        <RenderImages images={this.state.images}/>
+      </div>
+    );
+  }
 };
 
 
@@ -113,20 +107,20 @@ class RenderImages extends Component {
   }
 
   render() {
-    const { images } = this.props;
+    const {images} = this.props;
     console.log(images)
     return (
       <div className="gedit-container">
         {
           images.map((x) => {
             console.log(x);
-            return(
+            return (
               <div className="gedit-tile">
-                <img src={ x } className="gedit-img" alt=""/>
+                <img src={x} className="gedit-img" alt=""/>
               </div>
             )
           })
-        }   
+        }
       </div>
     );
   }
--- frontend/src/pages/pcomponents/EditLinks.js
+++ frontend/src/pages/pcomponents/EditLinks.js
@@ -1,159 +1,161 @@
-import React, { Component } from "react";
-import { updateLinks, getLinks } from "../../Api.js"
+import React, {Component} from "react";
+import {updateLinks, getLinks} from "../../Api.js"
 import "../../css/AboutLinks.css";
 
 export default class EditLinks extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            linksList: [{
-                title: "",
-                description: "",
-                link: ""
-            }],
-            isLoaded: false
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleRemove = this.handleRemove.bind(this);
-        this.handleAdd = this.handleAdd.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      linksList: [{
+        title: "",
+        description: "",
+        link: ""
+      }],
+      isLoaded: false
     }
-    
-    /* The method for having dynamic additional/removable input fields was taken and adapted from
-    https://www.cluemediator.com/add-or-remove-input-fields-dynamically-with-reactjs 
-    */
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-   handleChange(e, i) {
-    const { name, value } = e.target;
+  /* The method for having dynamic additional/removable input fields was taken and adapted from
+  https://www.cluemediator.com/add-or-remove-input-fields-dynamically-with-reactjs
+  */
+
+  handleChange(e, i) {
+    const {name, value} = e.target;
     const list = [...this.state.linksList];
     list[i][name] = value;
-    this.setState({ linksList: list });
-}
+    this.setState({linksList: list});
+  }
 
 
-    handleRemove(e, i) {
-        e.preventDefault();
-        const list = [...this.state.linksList];
-        list.splice(i, 1);
-        this.setState({ linksList: list });
+  handleRemove(e, i) {
+    e.preventDefault();
+    const list = [...this.state.linksList];
+    list.splice(i, 1);
+    this.setState({linksList: list});
+  }
+
+  handleAdd(e) {
+    e.preventDefault()
+    const list = [...this.state.linksList, {title: "", description: "", link: ""}]
+    this.setState({linksList: list});
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+
+    let list = []
+
+    for (var i = 0; i < this.state.linksList.length; i++) {
+      // If all fields are empty, the entry is not saved to the database
+      if (this.state.linksList[i].title !== "" && this.state.linksList[i].description !== "" && this.state.linksList[i].link !== "") {
+        list.push(this.state.linksList[i]);
+      }
+    }
+    ;
+
+
+    await this.setState({linksList: list});
+    updateLinks(this.state);
+    sessionStorage.setItem("activeTab", this.props.name);
+    window.location.reload();
+  }
+
+  async componentDidMount() {
+    let res
+
+    try {
+      res = await getLinks();
+    } catch (error) {
+      console.error(error);
     }
 
-    handleAdd(e) {
-        e.preventDefault()
-        const list = [...this.state.linksList, {title: "", description: "", link: ""}]
-        this.setState({ linksList: list});
-    }
+    if (res != null) {
+      this.setState({isLoaded: true})
+      if (res.data.links !== undefined && res.data.links.length !== 0) {
+        console.log(res.data.links);
+        let links = [];
+        (res.data.links).forEach((element, i) => {
+          links[i] = {title: element.title, description: element.description, link: element.link};
+        });
 
-    async handleSubmit(e) {
-        e.preventDefault();
-        
-        let list = []
-        
-        for(var i = 0; i < this.state.linksList.length; i++) {
-            // If all fields are empty, the entry is not saved to the database
-            if(this.state.linksList[i].title !== "" && this.state.linksList[i].description !== "" && this.state.linksList[i].link !== "") {
-                list.push(this.state.linksList[i]);
-            }
-        };
-        
-
-        await this.setState({linksList: list});
-        updateLinks(this.state);
-        sessionStorage.setItem("activeTab", this.props.name);
-        window.location.reload();
-    }
-
-    async componentDidMount() {
-        let res 
-        
-        try {
-            res = await getLinks();
-        } catch(error) {
-            console.error(error);
+        if (links[0].title !== "" && links[0].description !== "" && links[0].link !== "") {
+          this.setState({linksList: [...links, {title: "", description: "", link: ""}]})
         }
+      }
+    }
+  }
 
-        if(res != null) {
-            this.setState({ isLoaded: true })
-            if(res.data.links !== undefined && res.data.links.length !== 0) {
-                console.log(res.data.links);
-                let links = [];
-                (res.data.links).forEach((element, i) => {
-                    links[i] = {title: element.title, description: element.description, link: element.link};
-                });
-
-                if(links[0].title !== "" && links[0].description !== "" && links[0].link !== "") {
-                    this.setState({ linksList: [...links, { title: "", description: "", link: "" }] }) 
-                } 
-            }
-        }
-    } 
-   
-    render() {
-        // if(!this.state.isLoaded) {
-        //     return (
-        //         <div>
-        //             Loading...
-        //         </div>
-        //     )
-        // } else {
+  render() {
+    // if(!this.state.isLoaded) {
+    //     return (
+    //         <div>
+    //             Loading...
+    //         </div>
+    //     )
+    // } else {
+    return (
+      <div className="links-page">
+        <h1>
+          Edit Links
+        </h1>
+        <form>
+          {this.state.linksList.map((x, i) => {
             return (
-                <div className="links-page">
-                    <h1>
-                        Edit Links
-                    </h1>
-                    <form>
-                        { this.state.linksList.map((x, i) => {
-                            return (
-                                <div key={ i }>
-                                    <section className="links-page-view">
-                                        <label>
-                                            Title:
-                                        </label>
-                                        <input 
-                                            name="title"
-                                            value={ x.title }
-                                            onChange = { e => this.handleChange(e, i) }
-                                        />
-                                        <label>
-                                            Description:
-                                        </label>
-                                        <textarea 
-                                            className="text-box"
-                                            name="description"
-                                            value={ x.description }
-                                            onChange = { e => this.handleChange(e, i) }
-                                        />
-                                        <label>
-                                            Link:
-                                        </label>
-                                        <input
-                                            name="link"
-                                            value={ x.link }
-                                            onChange = { e => this.handleChange(e, i) }
-                                        />
-                                    </section>
-                                    <div className="add-remove-buttons">
-                                        { i !== this.state.linksList.length - 1 &&
-                                            <button className="save-btn" onClick={ e => this.handleRemove(e, i) } >
-                                                Remove
-                                            </button>
-                                        }
-                                        { this.state.linksList.length - 1 === i &&
-                                            <button className="save-btn" onClick={ this.handleAdd }>
-                                                Add Link
-                                            </button>
-                                        }
-                                    </div>
-                                </div>
-                            );
-                        })}
-                        <button className="save-btn" onClick={ this.handleSubmit }>
-                            Save
-                        </button>
-                    </form>
+              <div key={i}>
+                <section className="links-page-view">
+                  <label>
+                    Title:
+                  </label>
+                  <input
+                    name="title"
+                    value={x.title}
+                    onChange={e => this.handleChange(e, i)}
+                  />
+                  <label>
+                    Description:
+                  </label>
+                  <textarea
+                    className="text-box"
+                    name="description"
+                    value={x.description}
+                    onChange={e => this.handleChange(e, i)}
+                  />
+                  <label>
+                    Link:
+                  </label>
+                  <input
+                    name="link"
+                    value={x.link}
+                    onChange={e => this.handleChange(e, i)}
+                  />
+                </section>
+                <div className="add-remove-buttons">
+                  {i !== this.state.linksList.length - 1 &&
+                  <button className="save-btn" onClick={e => this.handleRemove(e, i)}>
+                    Remove
+                  </button>
+                  }
+                  {this.state.linksList.length - 1 === i &&
+                  <button className="save-btn" onClick={this.handleAdd}>
+                    Add Link
+                  </button>
+                  }
                 </div>
-            )
-        }
-    // }
+              </div>
+            );
+          })}
+          <button className="save-btn" onClick={this.handleSubmit}>
+            Save
+          </button>
+        </form>
+      </div>
+    )
+  }
+
+  // }
 }
